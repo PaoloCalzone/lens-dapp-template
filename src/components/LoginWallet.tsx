664a130e7119241477f2c39e25c6f06abce2243b
paolocalzone <paolocalzone@protonmail.com>
2022-10-21 15:54:00 +0200

    Start Login Wallet component

---
+++ src/components/LoginWallet.tsx
@@ -0,0 +1,7 @@
+import { FC, ReactNode, useCallback, useEffect } from "react";
+
+const LoginWallet: FC<{ children: any }> = ({ children }) => {
+  return <div></div>;
+};
+
+export default LoginWallet;
